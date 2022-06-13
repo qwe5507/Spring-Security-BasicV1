7f56d838d81ada769842d0b04a43d74549a24eb8
qwe5507 <apvmffkdls@gmail.com>
2022-06-13 23:22:59 +0900

    Initial commit

---
+++ src/main/java/com/example/securitybasicv1/Securitybasicv1Application.java
@@ -0,0 +1,13 @@
+package com.example.securitybasicv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Securitybasicv1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Securitybasicv1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/securitybasicv1/Securitybasicv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securitybasicv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Securitybasicv1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
