394e731d3e8d6275feec392d6b8b191c8c09178b
qwe5507 <apvmffkdls@gmail.com>
2022-06-25 17:47:52 +0900

    네이버 provider등록 및 네이버로그인 구현

--- src/main/java/com/example/securitybasicv1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/example/securitybasicv1/config/oauth/PrincipalOauth2UserService.java
@@ -3,6 +3,7 @@ package com.example.securitybasicv1.config.oauth;
 import com.example.securitybasicv1.config.auth.PrincipalDetails;
 import com.example.securitybasicv1.config.oauth.provider.FacebookUserInfo;
 import com.example.securitybasicv1.config.oauth.provider.GoogleUserInfo;
+import com.example.securitybasicv1.config.oauth.provider.NaverUserInfo;
 import com.example.securitybasicv1.config.oauth.provider.OAuth2UserInfo;
 import com.example.securitybasicv1.model.User;
 import com.example.securitybasicv1.repository.UserRepository;
@@ -13,6 +14,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
@@ -39,8 +42,11 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         } else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
             System.out.println("페이스북 로그인 요청");
             oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+            System.out.println("네이버 로그인 요청");
+            oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
         } else {
-            System.out.println("구글과 페이스북만 지원");
+            System.out.println("구글과 페이스북, 네이버만 지원");
         }
         String provider = oAuth2UserInfo.getProvider();
         String providerId = oAuth2UserInfo.getProviderId();
@@ -52,7 +58,7 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         User userEntity = userRepository.findByUsername(username);
 
         if (userEntity == null) {
-            System.out.println("로그인이 최초입니다.");
+            System.out.println("OAuth 로그인이 최초입니다.");
             userEntity = User.builder()
                     .username(username)
                     .password(password)
